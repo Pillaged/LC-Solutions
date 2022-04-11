ddb97e439b65570354c1a0f1a49d2bcb263d5a78
Josiah Luna <josiah.luna@live.com>
2022-04-11 11:44:11 -0700

    lc problems

--- LC Solutions/LC-152.py
+++ LC Solutions/LC-152.py
@@ -20,8 +20,15 @@ def maxProduct(nums) -> int:
 
     return maxprod
 
-
-
+"""
+        max_prod, min_prod, ans = nums[0], nums[0], nums[0]
+        for i in range(1, len(nums)):
+            x = max(nums[i], max_prod*nums[i], min_prod*nums[i])
+            y = min(nums[i], max_prod*nums[i], min_prod*nums[i])            
+            max_prod, min_prod = x, y
+            ans = max(max_prod, ans)
+        return ans"""
+        
 a = [1,2,3,4,5] #120
 b = [1,2,3,-4,-5] #120
 c = [-5,-4,0,3,2,1] #20
