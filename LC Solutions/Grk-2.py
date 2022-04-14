fcb52828779daaad2717511aada191597e8658b4
Josiah Luna <josiah.luna@live.com>
2022-04-14 14:51:42 -0700

    Grk #2

--- LC Solutions/Grk-2.py
+++ LC Solutions/Grk-2.py
@@ -1,16 +1,48 @@
 '''
 Problem Statement 
-Given an array of positive numbers and a positive number ‘S’, find the length of the smallest contiguous subarray whose sum is greater than or equal to ‘S’. Return 0, if no such subarray exists.
+Given an array of positive numbers and a positive number 'S', find the length of the smallest contiguous subarray whose sum 
+is greater than or equal to 'S'. Return 0, if no such subarray exists.
+
 Example 1:
 Input: [2, 1, 5, 2, 3, 2], S=7 
 Output: 2
 Explanation: The smallest subarray with a sum great than or equal to '7' is [5, 2].
+
 Example 2:
 Input: [2, 1, 5, 2, 8], S=7 
 Output: 1
 Explanation: The smallest subarray with a sum greater than or equal to '7' is [8].
+
 Example 3:
 Input: [3, 4, 1, 1, 6], S=8 
 Output: 3
 Explanation: Smallest subarrays with a sum greater than or equal to '8' are [3, 4, 1] or [1, 1, 6].
 '''
+
+from cgitb import small
+
+
+def smol(num_list, S):
+    smallestlen = len(num_list)+1
+    max = smallestlen
+    startpos = 0
+    sum_curr = 0
+    for endpos in range(0,len(num_list)):
+        sum_curr += num_list[endpos]
+        while endpos > startpos:
+            if sum_curr >= S:
+                smallestlen = min(smallestlen, (endpos-startpos)+1)
+                sum_curr -= num_list[startpos]
+                startpos += 1
+            else: break
+    if smallestlen < max:
+        return smallestlen 
+    return 0
+
+def main():
+    print(smol([2, 1, 5, 2, 3, 2], S=7))
+    print(smol([2, 1, 5, 2, 8], S=7))
+    print(smol([3, 4, 1, 1, 6], S=8))
+        
+if __name__ == "__main__":
+    main()
